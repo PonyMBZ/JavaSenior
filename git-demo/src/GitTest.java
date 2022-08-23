ebbd3ce4e4d3710aa1036a003eed19e1c3cbaa0a
Pony <2916378264@qq.com>
2022-08-23 10:40:05 +0800

    JavaEE Hot-fix First Commit

--- git-demo/src/GitTest.java
+++ git-demo/src/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("第一次创建");
         System.out.println("第一次修改");
+        System.out.println("hot-fix 第一次修改");
     }
 }
