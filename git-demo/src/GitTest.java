2cf1be43b77d501a2d6de8d11e155b1a80dafa73
Pony <2916378264@qq.com>
2022-08-23 10:56:58 +0800

    JavaEE master third Commit

--- git-demo/src/GitTest.java
+++ git-demo/src/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("第一次创建");
         System.out.println("第一次修改");
         System.out.println("hot-fix 第一次修改");
+        System.out.println("master 第二次修改");
     }
 }
